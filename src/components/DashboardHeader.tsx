608bbf4c978abc26f1aa2f156af1f2a27b84b1cf
William Lowrimore <wlowrimore@gmail.com>
2024-10-04 22:23:16 -0500

    some of the dashboard ui side nav added

--- src/components/DashboardHeader.tsx
+++ src/components/DashboardHeader.tsx
@@ -3,23 +3,26 @@ import React from "react";
 
 const DashboardHeader = () => {
   return (
-    <ul className="flex items-center justify-evenly">
-      <li className="font-semibold text-neutral-600 tracking-wider py-1 px-4 rounded-full mt-1 hover:bg-neutral-600 hover:text-white transition duration-300 cursor-pointer">
-        How It Works
-      </li>
-      <li className="font-semibold text-neutral-600 tracking-wider py-1 px-4 rounded-full mt-1 hover:bg-neutral-600 hover:text-white transition duration-300 cursor-pointer">
-        Case Studies
-      </li>
-      <li className="font-semibold text-neutral-600 tracking-wider py-1 px-4 rounded-full mt-1 hover:bg-neutral-600 hover:text-white transition duration-300 cursor-pointer">
-        Create Tasks
-      </li>
-      <li className="font-semibold text-neutral-600 tracking-wider py-1 px-4 rounded-full mt-1 hover:bg-neutral-600 hover:text-white transition duration-300 cursor-pointer">
-        Contact Us
-      </li>
-      <li className="font-semibold text-neutral-600 tracking-wider py-1 px-4 rounded-full mt-1 hover:bg-neutral-600 hover:text-white transition duration-300 cursor-pointer">
-        SignOut
-      </li>
-    </ul>
+    <aside className="flex flex-col w-[12rem] h-[77vh] min-h-[77vh] p-4 bg-purple-50 border-r border-neutral-700">
+      <ul className="w-full h-full space-y-3">
+        <li className="font-semibold text-[0.8rem] text-neutral-700 tracking-wider py-2 px-4 rounded-full mt-1 hover:bg-neutral-600 hover:text-white transition duration-300 cursor-pointer">
+          How It Works
+        </li>
+        <li className="font-semibold text-[0.8rem] text-neutral-700 tracking-wider py-2 px-4 rounded-full mt-1 hover:bg-neutral-600 hover:text-white transition duration-300 cursor-pointer">
+          Case Studies
+        </li>
+        <li className="font-semibold text-[0.8rem] text-neutral-700 tracking-wider py-2 px-4 rounded-full mt-1 hover:bg-neutral-600 hover:text-white transition duration-300 cursor-pointer">
+          Create Tasks
+        </li>
+
+        <li className=" font-semibold text-[0.8rem] text-neutral-700 tracking-wider py-2 px-4 rounded-full mt-1 hover:bg-neutral-600 hover:text-white transition duration-300 cursor-pointer">
+          Contact Us
+        </li>
+        <li className="fixed bottom-28 font-semibold text-sm text-neutral-600 tracking-wider py-2 px-4 rounded-full hover:bg-neutral-600 hover:text-white transition duration-300 cursor-pointer">
+          SignOut
+        </li>
+      </ul>
+    </aside>
   );
 };
 
